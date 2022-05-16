6e7f494a89aabe6077f02f332c05bbf3ac516e61
Sergey Masiuk <mayerror@gmail.com>
2022-05-16 22:19:25 +0300

    feat: add redirect if user is logged in

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './store';
 
 test('renders learn react link', () => {
   render(
     <MemoryRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </MemoryRouter>
   );
   const linkElement = screen.getByText(/Log in/i);
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Footer from './application/generalComponents/footer';
 import NotFound from './application/notFound';
 import Navigation from './application/generalComponents/navigation';
@@ -8,15 +8,18 @@ import MainPage from './application/mainPage';
 import BoardsPage from './application/boardsPage';
 import SignupPage from './application/authorizationForms/SignupPage/SignupPage';
 import LoginPage from './application/authorizationForms/LoginPage/LoginPage';
+import { useAppSelector } from './store';
 
 function App() {
+  const token = useAppSelector((state) => state.auth.token);
+
   return (
     <div className="App">
       {/* <Navigation /> */}
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/Login" element={<LoginPage />} />
-        <Route path="/Signup" element={<SignupPage />} />
+        <Route path="/Login" element={token ? <Navigate to="/" /> : <LoginPage />} />
+        <Route path="/Signup" element={token ? <Navigate to="/" /> : <SignupPage />} />
         <Route path="/Main" element={<MainPage />} />
         <Route path="/Boards" element={<BoardsPage />} />
         <Route path="/*" element={<NotFound />} />
