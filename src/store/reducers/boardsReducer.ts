a6bc9f9a16e16d6eae604dacf9ada17341f4caec
Petr Grachev <petr9ra@yandex.ru>
2022-05-17 23:52:29 +0300

    refactor: api requests using axios instance

--- src/store/reducers/boardsReducer.ts
+++ src/store/reducers/boardsReducer.ts
@@ -1,9 +1,19 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios, { AxiosResponse } from 'axios';
 import { Board, BoardBody, BoardList, ModalState } from '../../.d';
 
 export const baseUrl = 'https://kanban-rest-marina-team.herokuapp.com/';
 const tempToken =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJlZTViODM1ZC03NDQ3LTQwMDgtODg2OS1mZTgyZjgwNzhhZjgiLCJsb2dpbiI6InVzZXIxMyIsImlhdCI6MTY1MjQ3MDMxMn0.cF1yvtcgjdY2TDHbR3IiVRNWcZwbykQUJF5Z5HZsrn4';
+
+const boardAxios = axios.create({
+  baseURL: baseUrl,
+  timeout: 1000,
+  headers: {
+    Authorization: `Bearer ${tempToken}`,
+    'Content-Type': 'application/json',
+  },
+});
 
 export type BoardsState = {
   boardList: Board[];
@@ -28,13 +38,8 @@ export const createBoard = createAsyncThunk(
   'boardsReducer/createBoard',
   async (boardBody: BoardBody, { rejectWithValue }) => {
     try {
-      await fetch(`${baseUrl}boards`, {
-        method: 'POST',
-        headers: {
-          Authorization: `Bearer ${tempToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(boardBody),
+      await boardAxios.post(`boards`, {
+        ...boardBody,
       });
       return console.log('Board ' + boardBody.title + ' was created!');
     } catch (err) {
@@ -47,14 +52,8 @@ export const getBoards = createAsyncThunk(
   'boardsReducer/getBoards',
   async (_, { rejectWithValue }) => {
     try {
-      const response: Response = await fetch(`${baseUrl}boards`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${tempToken}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      return await response.json();
+      const response: AxiosResponse = await boardAxios.get(`boards`);
+      return await response.data;
     } catch (err) {
       rejectWithValue((err as Error).message);
     }
@@ -65,14 +64,8 @@ export const getBoardById = createAsyncThunk(
   'boardsReducer/getBoardById',
   async (id: string, { rejectWithValue }) => {
     try {
-      const response: Response = await fetch(`${baseUrl}boards/${id}`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${tempToken}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      return await response.json();
+      const response: AxiosResponse = await boardAxios.get(`boards/${id}`);
+      return await response.data;
     } catch (err) {
       rejectWithValue((err as Error).message);
     }
@@ -83,12 +76,7 @@ export const deleteBoardById = createAsyncThunk(
   'boardsReducer/deleteBoardById',
   async (id: string, { rejectWithValue }) => {
     try {
-      await fetch(`${baseUrl}boards/${id}`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${tempToken}`,
-        },
-      });
+      await boardAxios.delete(`boards/${id}`);
       return console.log('Board ' + id + ' was deleted!');
     } catch (err) {
       rejectWithValue((err as Error).message);
@@ -101,16 +89,9 @@ export const updateBoardById = createAsyncThunk(
   async (board: Board, { rejectWithValue }) => {
     try {
       const { id, title, description } = board;
-      await fetch(`${baseUrl}boards/${id}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${tempToken}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title,
-          description,
-        }),
+      await boardAxios.put(`boards/${id}`, {
+        title,
+        description,
       });
       return console.log('Board ' + title + ' was updated!');
     } catch (err) {
