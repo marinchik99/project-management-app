68f99e554d54f88fa875a241b44cbd712d185478
marinchik99 <marina_1999@tut.by>
2022-05-20 16:49:09 +0300

    fix: change route path and layout of board page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Footer from './application/generalComponents/footer';
 import NotFound from './application/notFound';
-import Navigation from './application/generalComponents/navigation';
 import BoardPage from './application/boardPage/boardPage';
 import WelcomePage from './application/welcomePage/welcomePage';
-<<<<<<< HEAD
-import BoardsPage from './application/boardsPage';
-=======
 import MainRoute from './application/mainRoute/MainRoute';
->>>>>>> 0323fe4743da9298cfebd7d7fa0f94ec443a40c7
 import SignupPage from './application/authorizationForms/SignupPage/SignupPage';
 import LoginPage from './application/authorizationForms/LoginPage/LoginPage';
 import { useAppSelector } from './store';
@@ -23,14 +18,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/Login" element={token ? <Navigate to="/" /> : <LoginPage />} />
-        <Route path="/Signup" element={token ? <Navigate to="/" /> : <SignupPage />} />
-<<<<<<< HEAD
-        <Route path="/Board" element={<BoardPage />} />
-        <Route path="/Boards" element={<BoardsPage />} />
-=======
-        <Route path="/Boards" element={<MainRoute />} />
->>>>>>> 0323fe4743da9298cfebd7d7fa0f94ec443a40c7
+        <Route path="/Login" element={token ? <Navigate to="/Boards" /> : <LoginPage />} />
+        <Route path="/Signup" element={token ? <Navigate to="/Boards" /> : <SignupPage />} />
+        <Route path="/Board" element={!token ? <Navigate to="/" /> : <BoardPage />} />
+        <Route path="/Boards" element={!token ? <Navigate to="/" /> : <MainRoute />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
       <Footer />
--- src/application/boardPage/boardPage.tsx
+++ src/application/boardPage/boardPage.tsx
@@ -1,91 +1,61 @@
-import { Container, Grid, Button, Stack, List, ListItem } from '@mui/material';
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Container, Grid, Button } from '@mui/material';
+import React, { useEffect } from 'react';
 import '../../css/boardPage.css';
 import AddIcon from '@mui/icons-material/Add';
+import Preloader from '../generalComponents/preloader';
+import { useAppDispatch, useAppSelector } from '../../store';
+import { getBoardById } from '../../store/reducers/boardsReducer';
 
 export default function BoardPage() {
+  const { isLoading, currentBoard } = useAppSelector(({ boardsReducer }) => boardsReducer);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getBoardById(currentBoard.id));
+  }, [currentBoard.id, dispatch]);
+
+  // const addColumn = () => {
+  //   const list = document.createElement('div');
+  // };
+
   return (
     <section className="board-page">
-      <Container maxWidth="xl" className="board-container">
-        <Grid
-          className="board-info"
-          container
-          spacing={1}
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Grid item xs={5}>
-            <h2 className="board-name">Board Name</h2>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <Container maxWidth="xl" className="board-container">
+          <Grid
+            className="board-info"
+            container
+            spacing={1}
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Grid item xs={6}>
+              <h2 className="board-name">Board Name</h2>
+            </Grid>
+            <Grid item xs={3}></Grid>
           </Grid>
-          <Grid item xs={5} className="button-container">
-            <NavLink to="/Boards" style={{ textDecoration: 'none' }}>
-              <Button className="button-to-boards" variant="contained">
-                На главную
-              </Button>
-            </NavLink>
-          </Grid>
-        </Grid>
-        <div className="work-container">
-          <div className="list-container">
-            <span className="list-title" contentEditable={true}>
-              Title
-            </span>
-            <div className="list">
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
-              <div className="list__item">dfgh</div>
+          <div className="work-container">
+            <div className="list-container">
+              <div className="list-block">
+                <h3 className="list-title">Title</h3>
+                <div className="list">
+                  <div className="list__item">Карточка с содержимым</div>
+                </div>
+                <div className="add-card">
+                  <AddIcon fontSize="small" />
+                  Добавить карточку
+                </div>
+              </div>
             </div>
-            <form>
-              <div></div>
-              {/* <Button className="button-add-column" variant="contained">
-                <AddIcon />
-                Добавить список
-              </Button> */}
-            </form>
+            <Button variant="outlined" className="button-add-column">
+              <AddIcon fontSize="small" />
+              Добавить колонку
+            </Button>
           </div>
-          <div className="list-container">Item 2</div>
-          <div className="list-container">Item 3</div>
-          <div className="list-container">Item 4</div>
-          <div className="list-container">Item 4</div>
-          <div className="list-container">Item 4</div>
-          <div className="list-container">Item 4</div>
-        </div>
-      </Container>
+        </Container>
+      )}
     </section>
   );
 }
