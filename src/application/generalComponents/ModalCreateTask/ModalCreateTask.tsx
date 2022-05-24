8abc060069bd940ac82e63d5fa624088497317e0
Sergey Masiuk <mayerror@gmail.com>
2022-05-24 11:44:03 +0300

    feat: add create modal form and validation

--- src/application/generalComponents/ModalCreateTask/ModalCreateTask.tsx
+++ src/application/generalComponents/ModalCreateTask/ModalCreateTask.tsx
@@ -1,5 +1,18 @@
-import { Box, Container, CssBaseline, Modal, ThemeProvider, Typography } from '@mui/material';
-import React from 'react';
+import { LoadingButton } from '@mui/lab';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  CssBaseline,
+  Modal,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import React, { useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { TaskType } from '../../../types/types';
 import theme from '../../../utils/themeSettings';
 
 type Props = {
@@ -11,22 +24,95 @@ const style = {
   position: 'absolute' as const,
   top: '50%',
   left: '50%',
+  width: 500,
+  ['@media (max-width:600px)']: {
+    width: '95%',
+  },
   transform: 'translate(-50%, -50%)',
   bgcolor: 'background.paper',
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
 
 export default function ModalCreateTask({ open, handleClose }: Props) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      title: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = async (data: Pick<TaskType, 'title' | 'description'>) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    reset();
+  }, [open]);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Modal open={open} onClose={handleClose}>
         <Box sx={style}>
-          <Container component="div" maxWidth="xl" sx={{ mb: 4 }}>
-            <Typography component="h1" variant="h4" align="center">
+          <Container component="div">
+            <Typography component="h1" variant="h5" align="center">
               Создание задачи
             </Typography>
+            <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{ my: 2 }}>
+              <Controller
+                name="title"
+                control={control}
+                rules={{
+                  required: { value: true, message: 'Заполните поле' },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    inputProps={{ 'data-testid': 'taskTitleC' }}
+                    margin="normal"
+                    fullWidth
+                    label="Заголовок"
+                    autoFocus
+                    error={!!errors.title}
+                    helperText={errors.title?.message}
+                  />
+                )}
+              />
+              <Controller
+                name="description"
+                control={control}
+                rules={{
+                  required: { value: true, message: 'Заполните поле' },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    inputProps={{ 'data-testid': 'taskDescC' }}
+                    margin="normal"
+                    fullWidth
+                    label="Описание задачи"
+                    autoFocus
+                    multiline
+                    minRows={4}
+                    sx={{ mb: 4 }}
+                    error={!!errors.description}
+                    helperText={errors.description?.message}
+                  />
+                )}
+              />
+              <ButtonGroup fullWidth variant="contained">
+                <LoadingButton data-testid="submitC" type="submit" fullWidth variant="contained">
+                  Создать
+                </LoadingButton>
+                <Button onClick={handleClose}>Закрыть</Button>
+              </ButtonGroup>
+            </Box>
           </Container>
         </Box>
       </Modal>
