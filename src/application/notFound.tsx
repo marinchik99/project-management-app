1d4d24dabae80a9b6d6666baa78f6c7f28058399
marinchik99 <marina_1999@tut.by>
2022-05-27 14:12:22 +0300

    feat: add preloader and style not-found page

--- src/application/notFound.tsx
+++ src/application/notFound.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/notFound.css';
 
 export default function NotFound() {
   return (
-    <div data-testid="notFound-page">
-      <h1 className="error"> 404 Not Found</h1>
-      <div className="redirect">
-        Go
-        <Link className="error-link" to="/">
-          Home
-        </Link>
+    <div className="notFound-page">
+      <div className="notFound-cont">
+        <h1 className="error"> This page not found</h1>
+        <div className="redirect">
+          Go to
+          <Link className="error-link" to="/Boards">
+            Main page
+          </Link>
+        </div>
       </div>
     </div>
   );
