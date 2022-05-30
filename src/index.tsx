1beef63dc54a4428faf3ce96ae428bac20f458d4
Sergey Masiuk <mayerror@gmail.com>
2022-05-30 22:56:56 +0300

    feat: add gh-pages deploy scripts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,11 +15,11 @@ injectStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
