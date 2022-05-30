906a4cdee1f2f6d8d26f518aa7ca51443ac414f9
Petr Grachev <petr9ra@yandex.ru>
2022-05-31 02:22:41 +0300

    feat: add burger menu

--- src/application/welcomePage/header.tsx
+++ src/application/welcomePage/header.tsx
@@ -8,7 +8,7 @@ import { setLanguage } from '../../store/reducers/settingsReducer';
 import { selectCurrentUser } from '../../store/reducers/authSlice';
 import { Box, Button, Container, Grid, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import '../../css/header.scss';
-import UserToolbar from './UserToolbar/UserToolbar';
+import UserToolbar from '../generalComponents/UserToolbar/UserToolbar';
 
 export default function Header() {
   const dispatch = useAppDispatch();
@@ -34,8 +34,28 @@ export default function Header() {
     dispatch(setLanguage(newLang));
   };
 
+  function toggleMenu(type: string) {
+    const burgerButton = document.querySelector('.burger');
+    const burgerNav = document.querySelector('.header__block-list');
+    const body = document.querySelector('body');
+
+    if (type === 'burger') {
+      burgerButton.classList.toggle('active');
+      burgerNav.classList.toggle('active');
+      body.classList.toggle('lock');
+    }
+    if (type === 'link') {
+      burgerButton.classList.remove('active');
+      burgerNav.classList.remove('active');
+      body.classList.remove('lock');
+    }
+  }
+
   return (
     <header className={scroll ? 'header header--sticky' : 'header'}>
+      <button className="burger" onClick={() => toggleMenu('burger')}>
+        <span className="burger__line"></span>
+      </button>
       <Container maxWidth="xl">
         <Grid
           container
@@ -43,24 +63,40 @@ export default function Header() {
           spacing={0}
           justifyContent="space-between"
           alignItems="center"
+          className="header__block-list"
         >
-          <Grid item>
+          <Grid item className="header__block">
+            <NavLink to="/" style={{ marginRight: '20px' }} onClick={() => toggleMenu('link')}>
+              <Button className="header__home-link" variant="text">
+                Remboard
+              </Button>
+            </NavLink>
             {token && (
               <>
-                <NavLink to="/boards" style={{ marginRight: '20px' }}>
+                <NavLink
+                  to="/boards"
+                  style={{ marginRight: '20px' }}
+                  onClick={() => toggleMenu('link')}
+                >
                   <Button variant="outlined">
                     <Trans i18nKey="header.boardsBtn">Доски</Trans>
                   </Button>
                 </NavLink>
                 <NavLink to="/boards">
-                  <Button variant="outlined" onClick={handleNewBoardClick}>
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      handleNewBoardClick();
+                      toggleMenu('link');
+                    }}
+                  >
                     <Trans i18nKey="header.createBoardBtn">Создать новую доску</Trans>
                   </Button>
                 </NavLink>
               </>
             )}
           </Grid>
-          <Grid item className="authoriz-butt-cont">
+          <Grid item className="header__block authoriz-butt-cont">
             <Box sx={{ display: 'flex', justifyContent: 'end', alignItems: 'center' }}>
               <ToggleButtonGroup
                 value={language}
